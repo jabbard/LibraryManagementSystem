d3da7ef13f7c791b03540cb8b4613cf2abd617e8
jabbard <Potter123>
2019-03-18 21:38:37 +0545

    working on login

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Admin',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,7 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-#CRISPY_TEMPLATE_PACK = '4.2.1'
+
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'LibraryManagementSystem','static'))
