053e165e75f7d1194951a710898ec05798d4338c
jabbard <Potter123>
2019-03-19 14:33:53 +0545

    initial commit for 19/3/2019

--- Admin/models.py
+++ Admin/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Books(models.Model):
     book_id = models.CharField(primary_key=True, max_length=10)
     book_name = models.CharField(max_length=255, default="")
-    ISBN = models.CharField(max_length=100, default="")
+    isbn_regex = RegexValidator(regex='^[0-9]{10}|[0-9]{13}$', message="The ISBN number should be 10 or 13 digit in length.")
+    ISBN = models.CharField(validators=[isbn_regex],max_length=100, default="")
     description = models.TextField(default="")
     author = models.CharField(max_length=100, default="")
     publisher = models.CharField(max_length=255, default="")
@@ -15,3 +17,31 @@ class Books(models.Model):
 
     def __str__(self):
         return self.book_id
+
+class Book_Number(models.Model):
+    b_id = models.CharField(primary_key=True, max_length=10)
+    book_id = models.ForeignKey(Books, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.b_id
+
+class Faculty(models.Model):
+    f_id = models.CharField(primary_key=True, max_length=10)
+    f_name = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.f_id
+
+class Students(models.Model):
+    s_id = models.CharField(primary_key=True, max_length=10)
+    st_name = models.CharField(max_length=100, default="")
+    ph_validator = RegexValidator(regex='^(98[0-9]{8}|97[0-9]{8})', message="Invalid Phone no.")
+    ph_num = models.CharField(validators=[ph_validator],max_length=15, default="")
+    email_validator = RegexValidator(regex='^([\w]*[\w\.]*(?!\.)@islingtoncollege.edu.np)', message="Invalid email address!")
+    email = models.EmailField(validators=[email_validator],default="")
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.s_id
+
+
